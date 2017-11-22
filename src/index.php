40868ff2b9a1f65252959d6cd07243f2b30a62bd
enouwin <ciprin.loris@gmail.com>
2017-11-22 09:03:01 +0100

    add firebase initialize

--- src/index.php
+++ src/index.php
@@ -1,8 +1,21 @@
 <html>
-  <head>
-    <title>Test PHP</title>
-  </head>
-  <body>
-    <?php echo '<p>Bonjour le monde</p>'; ?>
-  </body>
+    <head>
+        <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
+        <script>
+            // Initialize Firebase
+            var config = {
+                apiKey: "AIzaSyCzm2EaWLkgfxCRsq4kOgZUIQvofFWjtE4",
+                authDomain: "myblog-5b6bf.firebaseapp.com",
+                databaseURL: "https://myblog-5b6bf.firebaseio.com",
+                projectId: "myblog-5b6bf",
+                storageBucket: "",
+                messagingSenderId: "190365729125"
+            };
+            firebase.initializeApp(config);
+        </script>
+        <title>Test PHP</title>
+    </head>
+    <body>
+        <?php echo '<p>Bonjour le monde</p>'; ?>
+    </body>
 </html>
