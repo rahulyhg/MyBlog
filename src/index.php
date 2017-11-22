97ad24682fc96720a9840413e001b1db178762c7
enouwin <ciprin.loris@gmail.com>
2017-11-22 11:52:57 +0100

    firebase integrate

--- src/index.php
+++ src/index.php
@@ -1,25 +1,44 @@
 <html>
-    <head>
-        <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
-        <script>
-            // Initialize Firebase
-            var config = {
-                apiKey: "AIzaSyCzm2EaWLkgfxCRsq4kOgZUIQvofFWjtE4",
-                authDomain: "myblog-5b6bf.firebaseapp.com",
-                databaseURL: "https://myblog-5b6bf.firebaseio.com",
-                projectId: "myblog-5b6bf",
-                storageBucket: "",
-                messagingSenderId: "190365729125"
-            };
-            firebase.initializeApp(config);
-        </script>
-        <title>Test PHP</title>
-    </head>
-    <body>
-        <?php
-            echo '<p>Bonjour le monde</p>';
-            use Kreait\Firebase\Factory;
-            $firebase = (new Factory)->create();
-        ?>
-    </body>
+<head>
+    <title>Test PHP</title>
+    <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
+    <script>
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyCzm2EaWLkgfxCRsq4kOgZUIQvofFWjtE4",
+            authDomain: "myblog-5b6bf.firebaseapp.com",
+            databaseURL: "https://myblog-5b6bf.firebaseio.com",
+            projectId: "myblog-5b6bf",
+            storageBucket: "myblog-5b6bf.appspot.com",
+            messagingSenderId: "190365729125"
+        };
+        firebase.initializeApp(config);
+    </script>
+</head>
+<body>
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+$serviceAccount =
+    ServiceAccount::fromJsonFile(__DIR__ . '/../myblog-5b6bf-firebase-adminsdk-fvoav-26320f1a36.json');
+$apiKey = 'AIzaSyCzm2EaWLkgfxCRsq4kOgZUIQvofFWjtE4';
+
+$firebase = (new Factory)
+    ->withServiceAccountAndApiKey($serviceAccount, $apiKey)
+    ->create();
+$database = $firebase->getDatabase();
+$reference = $database->getReference('test/user');
+$reference->set([
+    'name' => 'My Application',
+    'emails' => [
+        'support' => 'support@domain.tld',
+        'sales' => 'sales@domain.tld',
+    ],
+    'website' => 'https://app.domain.tld',
+]);
+?>
+</body>
 </html>
